4a9fbc869dccacde4d19d2a5ff1b61d015e5ad9f
Just-ich <edik01101997@mail.ru>
2022-05-19 17:25:58 +0300

    Чтение и запись из\в файл v0.1

--- src/WritAndRead/Main.java
+++ src/WritAndRead/Main.java
@@ -1,65 +1,63 @@
 package WritAndRead;
 
-import WritAndRead.Exception.SimpleException;
-
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException, NullPointerException {
+    public static void main(String[] args) throws IOException, NullPointerException {
 
 
-       // File file = new File("P.txt");
+        // записываем данные в файл
+        File file1 = new File("P.txt");
+        PrintWriter pw = new PrintWriter(file1);
+        pw.println("*************\n" +
+                "* sdfd      *\n" +
+                "54*         *\n" +
+                "*           *\n" +
+                "*           *\n" +
+                "34gdsv*******\n" +
+                "3876587678sv*******\n" +
+                "*************");
+        pw.close();
 
 
-//        PrintWriter pw = new PrintWriter(file);
-//
-//        pw.println("*************\n" +
-//                "* sdfd      *\n" +
-//                "54*         *\n" +
-//                "*           *  \n" +
-//                "*           *\n" +
-//                "34gdsv*******\n" +
-//                "*************");
-//        pw.close();
+        // Узнаём кол-во строк в файле
+        BufferedReader reader = new BufferedReader(new FileReader("P.txt"));
+        int lines = 0;
+        while (reader.readLine() != null) lines++;
+        reader.close();
 
+
+        //создаём ArrayList; массив строк и записываем каждую строку в массив строк
         File file = new File("P.txt");
         ArrayList<String> list = new ArrayList<>();
         Scanner scanner = new Scanner(file);
-        System.out.println(file);
-        String[] str = new String[8];  // цифра 8 это кол-во строк в файле
+        String[] str = new String[lines];
         int counter = 0;
         while (scanner.hasNextLine()) {
-            //list.add(scanner.nextLine());
             str[counter] = scanner.nextLine();
-
             counter++;
         }
         scanner.close();
 
 
+        //проверяем массив строк на наличие букв и цифр
         int numberChar=0, numberLine;
-        for (int j = 0; j < str.length; j++) {
-                char[] strToArray = str[numberChar].toCharArray();
-                list.add(str[numberChar]);
-                for (int k = 0; k < str[0].length(); k++) {
-                    if (Character.isLetter(strToArray[k]) || Character.isDigit(strToArray[k]) ) {
+        for (int j = 0; j < str.length; j++) {                                                 // выбор строки прохода
+                char[] strToArray = str[numberChar].toCharArray();                             // создание массива чаров из строки
+                list.add(str[numberChar]);                                                     // записываем строку в ArrayList
+                for (int k = 0; k < str[0].length(); k++) {                                    // проверяем массив чаров на наличие букв и цифр
+                    if (Character.isLetter(strToArray[k]) || Character.isDigit(strToArray[k]) ) {   // сама проверка на наличие букв и цифр
                         numberLine=j+1;
-                        System.out.println("Есть буква, цифра --- номер строки "+numberLine);
-                        list.remove(str[numberChar]);
+                        System.out.println("Есть буква, цифра --- номер строки "+numberLine);      // вывод строки в которой есть буква или число
+                        list.remove(str[numberChar]);                                              // удаление строки с буквой или цифрой
                         break;
                     }
                 }
             numberChar++;
         }
-        System.out.println("\n"+list);
+        System.out.println("\n"+list);  // вывод конечного результата
     }
 }
 
