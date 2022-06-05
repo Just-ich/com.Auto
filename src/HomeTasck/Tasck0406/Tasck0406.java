09db543c8ee29b7103660fe974eea3ec7a3e3c9d
Just-ich <edik01101997@mail.ru>
2022-06-05 11:07:43 +0300

    HomeTasck04/06v0.1

--- src/HomeTasck/Tasck0406/Tasck0406.java
+++ src/HomeTasck/Tasck0406/Tasck0406.java
@@ -1,58 +1,73 @@
 package HomeTasck.Tasck0406;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class Tasck0406 {    public void homeTasck(){
-    //        //ДЗ
+public class Tasck0406 {
+    public void homeTasck() {
+        //        //ДЗ
 //        // 1)Задана последовательность, состоящая только из символов ‘>’, ‘<’ и ‘-‘.
 //        // Требуется найти количество стрел, которые спрятаны в этой последовательности.
 //        // Стрелы – это подстроки вида ‘>>-->’ и ‘<--<<’.
-//
-//
-//        String str = ">>--><--<<<--<<>>--><--<<<--<<>>--><--<<<--<<>>--><--<<<--<<";
-//        String strResult = "";
-//        String left = "<--<<";
-//        String right = ">>-->";
-//
-//        int countLeft = 0, countRight = 0;
-//        char[] chars = str.toCharArray();
-//        for (int i = 0; i < chars.length; i += left.length()) {   // ч/з каждые кол-во символов стрелы добавляем пробел
-//            for (int j = 0; j < left.length(); j++) {             // для последуещего разбиения строки на подстроки
-//                strResult += chars[j + i];
-//            }
-//            strResult += " ";
-//        }
-//
-//
-//        List<String> list = new ArrayList();
-//        for (int i = 0; i < 1; i++) {
-//            list = List.of(strResult.split(" "));
-//            for (var x : list) {
-//                if (x.equals(left)) {
-//                    countLeft++;
-//                }
-//                if (x.equals(right)) {
-//                    countRight++;
-//                }
-//            }
-//        }
-//        System.out.println(left + " " + countLeft + " шт.");
-//        System.out.println(right + " " + countRight + " шт.");
-//        System.out.println(list);
 
 
-    //2)Дано число n, найти кол-во простых чисел в промежутке от n до 2n(не включая их) - Постулат Бертрана
+        String str = ">>--><--<<<--<<>>--><--<<<--<<";
+        String strResult = "";
+        String left = "<--<<";
+        String right = ">>-->";
 
-
-    int chislo;
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Введите число ");
-    chislo =scanner.nextInt();
-    for (int i = chislo+1; i < chislo*2; i++) {
-        if (i%2==0 && i!=2 || i==1 || i%3==0 || i%5==0){
-            continue;
+        int countLeft = 0, countRight = 0;
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i += left.length()) {   // ч/з каждые кол-во символов стрелы добавляем пробел
+            for (int j = 0; j < left.length(); j++) {             // для последуещего разбиения строки на подстроки
+                strResult += chars[j + i];
+            }
+            strResult += " ";
         }
-        System.out.println(i);
+
+
+
+        List<String> list = new ArrayList();
+        for (int i = 0; i < 1; i++) {
+            list = List.of(strResult.split(" "));
+            for (var x : list) {
+                if (x.equals(left)) {
+                    countLeft++;
+                }
+                if (x.equals(right)) {
+                    countRight++;
+                }
+            }
+        }
+        System.out.println(left + " " + countLeft + " шт.");
+        System.out.println(right + " " + countRight + " шт.");
+        System.out.println(list);
+
+
+        //2)Дано число n, найти кол-во простых чисел в промежутке от n до 2n(не включая их) - Постулат Бертрана
+
+
+//        int chislo;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите число ");
+//        chislo = scanner.nextInt();
+//        for (int i = chislo + 1; i < chislo * 2; i++) {
+//            if (i % 2 == 0 && i != 2 || i == 1) {
+//                continue;
+//            }
+//            int neChetNumber = 3;
+//            boolean check = true;
+//            while (neChetNumber * neChetNumber <= i) {
+//                if (i % neChetNumber == 0) {
+//                    check = false;
+//                    break;
+//                }
+//                neChetNumber += 2;
+//            }
+//            if (check) {
+//                System.out.println(i);
+//            }
+//        }
     }
-}
 }
