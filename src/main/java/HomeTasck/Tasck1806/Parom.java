1dc89ca5af6b18c4456b5375c4a731d0d66b2636
Just-ich <edik01101997@mail.ru>
2022-06-18 19:25:33 +0300

    first Thread

---
+++ src/main/java/HomeTasck/Tasck1806/Parom.java
@@ -0,0 +1,10 @@
+package HomeTasck.Tasck1806;
+
+public class Parom {
+
+    double maxHeftParom;// грузоподъёмность 100т
+
+    public Parom(double maxHeftParom) {
+        this.maxHeftParom = maxHeftParom;
+    }
+}
